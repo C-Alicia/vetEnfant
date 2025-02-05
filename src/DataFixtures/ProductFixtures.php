f3aa32d20cf58776afaed86ae6d6692db575b110
aliciac <alicia.charef@hotmail.fr>
2025-02-05 17:03:07 +0100

    feat:

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -39,7 +39,7 @@ class ProductFixtures extends Fixture
         ],
         [
             'name' => 'Pyjamas de motif père-Noël',
-            'gender' => Gender::UNISEX,
+            'gender' => Gender::UNISEXE,
             'state' => State::TRESBONETAT,
             'price' => 19.99,
             'size' => 9,
